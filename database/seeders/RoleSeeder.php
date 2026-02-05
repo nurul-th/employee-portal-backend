8efe0a45f34df7e02915b6aa381fd3792002a17e
Nurul Taha <nurultaha.takaful@gmail.com>
2026-02-05 17:19:28 +0800

    Seeder for database and Roles

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    //Run the database seed
+    public function run(): void
+    {
+        Role::firstOrCreate(['name' => 'Admin']);
+        Role::firstOrCreate(['name' => 'Manager']);
+        Role::firstOrCreate(['name' => 'Employee']);
+    }
+}
