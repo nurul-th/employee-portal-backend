ed21aed39ec0d628f194e538d487a964de17ffa0
Nurul Taha <nurultaha.takaful@gmail.com>
2026-02-08 11:58:50 +0800

    Seed Admin user with role working auth

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,15 +4,29 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
+use App\Models\User;
 
 class RoleSeeder extends Seeder
 {
     //Run the database seed
     public function run(): void
     {
-        Role::firstOrCreate(['name' => 'Admin']);
-        Role::firstOrCreate(['name' => 'Manager']);
-        Role::firstOrCreate(['name' => 'Employee']);
+        $adminRole = Role::firstOrCreate(['name' => 'Admin']);
+        $managerRoler = Role::firstOrCreate(['name' => 'Manager']);
+        $employeeRole = Role::firstOrCreate(['name' => 'Employee']);
+
+        // Create Admin User
+        $admin = User::firstOrCreate(
+            ['email' => 'admin@example.com'],
+            [
+                'name' => 'Admin User',
+                'password' => Hash::make('password'),
+                'department_id' => 1,
+            ]
+        );
+
+        $admin->assignRole($adminRole);
     }
 }
